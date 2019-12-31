9a80fd506c1967930ef6e35d1df50d6b4a7669d4
mathew-fleisch <mathew.fleisch@gmail.com>
2019-12-30 21:39:57 -0800

    Add fade in/out

--- scripts/processor/merge-audio-video.sh
+++ scripts/processor/merge-audio-video.sh
@@ -22,6 +22,7 @@ get_duration_in_seconds() {
 
 MUSIC_LENGTH=$(get_duration_in_seconds $1)
 VIDEO_LENGTH=$(get_duration_in_seconds $2)
+VIDEO_FADE=$((MUSIC_LENGTH - 2))
 
 echo "Music: $MUSIC_LENGTH | Video: $VIDEO_LENGTH"
 
@@ -35,3 +36,6 @@ echo "Add audio: $1"
 AUDIO=$(echo $FAST | sed -e 's/_fast/_audio/g')
 ffmpeg -i $FAST -i $1 -c copy -map 0:v:0 -map 1:a:0 $AUDIO
 
+echo "Adding fade in/out"
+FADE=$(echo $AUDIO | sed -e 's/_audio/_fade/g')
+ffmpeg -i $AUDIO -filter:v "fade=in:st=0:d=2, fade=out:st=${VIDEO_FADE}:d=2" $FADE
